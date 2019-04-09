6ce2a959c9d729ade586ba62da428e1dca41d841
NovelleP <46993394+NovelleP@users.noreply.github.com>
2019-04-09 16:58:23 +0200

    Update NumberOfBoxes.java

--- src/numberOfBoxes/NumberOfBoxes.java
+++ src/numberOfBoxes/NumberOfBoxes.java
@@ -2,55 +2,35 @@ package numberOfBoxes;
 
 public class NumberOfBoxes {
 	
-	public int getNumberOfBoxes(int[] itemSizes, int boxSize) {
+	public int[] getNumberOfBoxes(int[] itemSizes, int boxSize) {
 		int[] boxes = new int[itemSizes.length]; //used capacity
 		IntWrapper lastBox = new IntWrapper(0);
 		IntWrapper minBoxes = new IntWrapper(itemSizes.length);
+		int[] itemBoxes = new int[itemSizes.length];
 		
-		calculateNumberOfBoxes(0, itemSizes, boxes,  lastBox, minBoxes, boxSize);
-		
-//		int[] itemBoxes = new int[itemSizes.length];
-//		calculateBoxOfEachItem(0, itemSizes, itemBoxes, boxes,  lastBox, minBoxes, boxSize);
-//		
-		return minBoxes.getValue();
+		calculateBoxOfEachItem(0, itemSizes, itemBoxes, boxes,  lastBox, minBoxes, boxSize);
+		return itemBoxes;
 	}
 	
-	private void calculateNumberOfBoxes(int n, int[] itemSizes, int[] boxes, IntWrapper lastBox, IntWrapper minBoxes, int boxSize) {
-		if(n == itemSizes.length) {
-			if(lastBox.getValue() < minBoxes.getValue())
-				minBoxes.setValue(lastBox.getValue());
-		}
-		
-		else {
-			int aux = lastBox.getValue(); //actual lastBox 
-			for(int i=0; i<=aux; i++) {
-				if(boxes[i] + itemSizes[n] <= boxSize) {
-					boxes[i] += itemSizes[n];
-					if(i+1 > lastBox.getValue())
-						lastBox.setValue(i+1);
-					calculateNumberOfBoxes(n+1, itemSizes, boxes, lastBox, minBoxes, boxSize);
-				}
-			}
-		}
-	}
-	
-	
-	/*this method also gets the box that stores each item*/
+
 	private void calculateBoxOfEachItem(int n, int[] itemSizes, int[] itemBoxes, int[] boxes, IntWrapper lastBox, IntWrapper minBoxes, int boxSize) {
 		if(n == itemBoxes.length) {
-			if(lastBox.getValue() < minBoxes.getValue())
-				minBoxes.setValue(lastBox.getValue());
+			if((lastBox.getValue()+1) < minBoxes.getValue()) {
+				minBoxes.setValue(lastBox.getValue()+1);
+			}
 		}
 		
 		else {
 			int aux = lastBox.getValue();
 			for(int i=0; i<=aux && i<boxes.length; i++) {
-				if(boxes[i] + itemSizes[n] <= boxSize) {
+				if((boxes[i] + itemSizes[n]) <= boxSize) {
 					boxes[i] += itemSizes[n];
-					itemBoxes[n] = i+1;
-					if(i+1 > lastBox.getValue())
-						lastBox.setValue(i+1);
+					itemBoxes[n] = i;
 					calculateBoxOfEachItem(n+1, itemSizes, itemBoxes, boxes, lastBox, minBoxes, boxSize);
+				}
+				else if(i == lastBox.getValue()){
+					lastBox.setValue(i+1);
+					calculateBoxOfEachItem(n, itemSizes, itemBoxes, boxes, lastBox, minBoxes, boxSize);
 				}
 			}
 		}
