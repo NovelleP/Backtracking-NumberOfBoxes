b59a64386a089f7a762051922d48b9e6477dff9b
NovelleP <46993394+NovelleP@users.noreply.github.com>
2019-04-09 16:59:10 +0200

    Update App.java

--- src/numberOfBoxes/App.java
+++ src/numberOfBoxes/App.java
@@ -5,10 +5,12 @@ public class App {
 	public static void main(String[] args) {
 		NumberOfBoxes b = new NumberOfBoxes();
 		int[] itemSizes = {7,10,4,2,15,7,15};
-		int boxSize = 18;
-		int boxes = b.getNumberOfBoxes(itemSizes, boxSize);
-		System.out.println(boxes);
+		int boxSize = 15;
+		int[] boxes = b.getNumberOfBoxes(itemSizes, boxSize);
 		
+		for(int n: boxes)
+			System.out.print(n + " ");
+		System.out.println();
 
 	}
 
