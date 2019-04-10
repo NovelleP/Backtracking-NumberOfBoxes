59b01a155d8819082d4c0075f8027e22ee6b01b3
Alberto Novelle <alberto109@hotmail.es>
2019-04-10 05:05:48 +0200

    update class NumberOfboxes

--- src/numberOfBoxes/NumberOfBoxes.java
+++ src/numberOfBoxes/NumberOfBoxes.java
@@ -1,39 +1,55 @@
 package numberOfBoxes;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class NumberOfBoxes {
 	
 	public int[] getNumberOfBoxes(int[] itemSizes, int boxSize) {
 		int[] boxes = new int[itemSizes.length]; //used capacity
-		IntWrapper lastBox = new IntWrapper(0);
-		IntWrapper minBoxes = new IntWrapper(itemSizes.length);
+		IntWrapper minBoxes = new IntWrapper(itemSizes.length+1);
 		int[] itemBoxes = new int[itemSizes.length];
+		int[] bestItemBoxes = new int[itemSizes.length];
 		
-		calculateBoxOfEachItem(0, itemSizes, itemBoxes, boxes,  lastBox, minBoxes, boxSize);
-		return itemBoxes;
+		calculateBoxOfEachItem(0, itemSizes, itemBoxes, boxes,  minBoxes, boxSize, bestItemBoxes);
+		return bestItemBoxes;
 	}
 	
 
-	private void calculateBoxOfEachItem(int n, int[] itemSizes, int[] itemBoxes, int[] boxes, IntWrapper lastBox, IntWrapper minBoxes, int boxSize) {
+	private void calculateBoxOfEachItem(int n, int[] itemSizes, int[] itemBoxes, int[] boxes, IntWrapper minBoxes, int boxSize, int[] bestItemBoxes) {
 		if(n == itemBoxes.length) {
-			if((lastBox.getValue()+1) < minBoxes.getValue()) {
-				minBoxes.setValue(lastBox.getValue()+1);
+			int c = countBoxes(itemBoxes); //need optimization. Try save number of box used
+			if(c < minBoxes.getValue()) {
+				minBoxes.setValue(c);
+				copyArray(itemBoxes, bestItemBoxes);
 			}
-		}
-		
+		}		
 		else {
-			int aux = lastBox.getValue();
-			for(int i=0; i<=aux && i<boxes.length; i++) {
+			for(int i=0; i<=n; i++) {
 				if((boxes[i] + itemSizes[n]) <= boxSize) {
 					boxes[i] += itemSizes[n];
 					itemBoxes[n] = i;
-					calculateBoxOfEachItem(n+1, itemSizes, itemBoxes, boxes, lastBox, minBoxes, boxSize);
-				}
-				else if(i == lastBox.getValue()){
-					lastBox.setValue(i+1);
-					calculateBoxOfEachItem(n, itemSizes, itemBoxes, boxes, lastBox, minBoxes, boxSize);
+					calculateBoxOfEachItem(n+1, itemSizes, itemBoxes, boxes, minBoxes, boxSize, bestItemBoxes);
+					boxes[i] -= itemSizes[n];
 				}
 			}
 		}
+	}
+	
+	
+	private int countBoxes(int[] itemBoxes) {
+		Set<Integer> set = new HashSet<>();
+		int count = 0;
+		for(int n: itemBoxes)
+			if(set.add(n))
+				count++;
+		return count;
+	}
+
+
+	private void copyArray(int[] itemBoxes, int[] bestItemBoxes) {
+		for(int i=0; i<itemBoxes.length; i++)
+			bestItemBoxes[i] = itemBoxes[i];		
 	}
 
 }
